9b67d3cd214f77c96c712d48aba8191eba0c8a14
Andrey Titov <andreit@videotron.ca>
2016-09-15 21:18:52 -0400

    Adding SFML (version 2.4.0) compatible with VS2013

--- COMP_345_Project/Main.cpp
+++ COMP_345_Project/Main.cpp
@@ -1,11 +1,26 @@
-#include <iostream>
-
-using namespace std;
-
-// Test line
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	cout << "Test" << endl;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	window.setFramerateLimit(60); // Sets the frame rate
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) // Game loop
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
