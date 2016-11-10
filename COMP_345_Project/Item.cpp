c81edbdbf23fa29f21b14ea4ae1f285740a7a27a
Jonathan Mastromonaco <Jonathan Mastromonaco>
2016-11-10 14:31:04 -0500

    Added more docs, Fixed Item read function

--- COMP_345_Project/Item.cpp
+++ COMP_345_Project/Item.cpp
@@ -159,6 +159,9 @@ bool Item::validateItem()
 	}
 	return true;
 }
+
+//! Method that converts an Item object into a string representation
+//! return: string that represents an Item Object
 string Item::serializeItem() {
 	string itemString = "";
 	itemString += itemName;
@@ -173,6 +176,7 @@ string Item::serializeItem() {
 	}
 	return itemString;
  }
+
 
 ostream& operator<<(ostream& stream, const Item& item){
 
@@ -197,181 +201,8 @@ ostream& operator<<(ostream& stream, const Item& item){
 }
 
 
-//! Method to add item to backpack
-//! @param Character to add item to its backpack
-static void createItem(Character &chr) {
 
-	string name = ""; //! Item name
-	int itemType = (int)ItemType::UNSPECIFIED; //! item type
-	int choiceItem = 0; //! Item or weapon
-	int range = 0; //! Range of weapon
-	vector<Buff> vecBuff(0); //! Vector for buff
-
-	//! Ask for weapon or item
-	cout << "\nDo you want to create an Item - 0 or a Weapon - 1? ";
-	cin >> choiceItem;
-	//! check choice
-	while (choiceItem < 0 || choiceItem>1) { //!  ask to continue enter if incorrect
-		cout << "\nIncorrect choice. Please choose only 0 or 1: ";
-		cin >> choiceItem;
-	}
-
-	if (choiceItem == 0) { //! If item or weapon
-
-		cout << "\nLet's create a new Item!!!";
-
-		cout << "\nEnter the Item Name: ";
-		cin.ignore();
-		getline(cin, name); //! Get name
-
-		//! output all posisible type and ask to choose
-		cout << "Enter the Item type no. from the list provided below:\n";
-		cout << (int)ItemType::HELMET << " - " << ItemType::HELMET << "\n"
-			<< (int)ItemType::ARMOR << " - " << ItemType::ARMOR << "\n"
-			<< (int)ItemType::SHIELD << " - " << ItemType::SHIELD << "\n"
-			<< (int)ItemType::RING << " - " << ItemType::RING << "\n"
-			<< (int)ItemType::BELT << " - " << ItemType::BELT << "\n"
-			<< (int)ItemType::BOOTS << " - " << ItemType::BOOTS << "\n"
-			<< (int)ItemType::WEAPON << " - " << ItemType::WEAPON << "\n";
-		cout << "Enter choice: ";
-		cin >> itemType;
-		//! Continue to ask if choice is incorrectly entered
-		while (itemType < 0 || itemType>6) {
-			cout << "\nIncorrect choice. Please enter again: ";
-			cin >> itemType;
-		}
-
-		int choiceBuff = 0; //! Buff choice
-		while (choiceBuff != -1) { //! While player wants to continue add buff
-			cout << "\nChoose the buffs for this item from the list below: \n";
-			//! Display all possible buffs
-			cout << (int)BuffType::INTELLIGENCE << " - " << BuffType::INTELLIGENCE << "\n"
-				<< (int)BuffType::WISDOM << " - " << BuffType::WISDOM << "\n"
-				<< (int)BuffType::ARMOR_CLASS << " - " << BuffType::ARMOR_CLASS << "\n"
-				<< (int)BuffType::STRENGTH << " - " << BuffType::STRENGTH << "\n"
-				<< (int)BuffType::CHARISMA << " - " << BuffType::CHARISMA << "\n"
-				<< (int)BuffType::CONSTITUTION << " - " << BuffType::CONSTITUTION << "\n"
-				<< (int)BuffType::DEXTERITY << " - " << BuffType::DEXTERITY << "\n"
-				<< (int)BuffType::ATTACK_BONUS << " - " << BuffType::ATTACK_BONUS << "\n"
-				<< (int)BuffType::DAMAGE_BONUS << " - " << BuffType::DAMAGE_BONUS << "\n"
-				<< "-1 to stop\n";
-			cout << "Enter choice: ";
-			cin >> choiceBuff; //! Enter buff choice
-
-			//! Check for buff choice. Ask again if incorrectly entered
-			while (choiceBuff != -1 && (choiceBuff < 0 || choiceBuff>8)) {
-				cout << "\nIncorrect Input. Please enter a correct number: ";
-				cin >> choiceBuff;
-			}
-
-			//! If do not want to exxit
-			if (choiceBuff != -1) {
-				int amount = 0;
-				//! Ask for buff amount
-				cout << "Enter buff's amount: ";
-				cin.ignore();
-				cin >> amount;
-				//! Put in vector for buff
-				vecBuff.push_back(Buff((BuffType)choiceBuff, amount));
-			}
-
-		}
-	}
-	else {
-		//! Create a weapon
-		cout << "\nLet's create a new Weapon!!!";
-		cout << "\nEnter the Weapon Name: ";
-		cin.ignore();
-		getline(cin, name); //! Get weapon name
-
-		cout << "\nPlease enter the weapon's range: ";
-		cin >> range; //! Get weapon range
-
-		int choiceBuff = 0;
-		while (choiceBuff != -1) { //! Ask for buff choice
-			cout << "\nChoose the buffs for this weapon from the list below: \n";
-			cout << (int)BuffType::INTELLIGENCE << " - " << BuffType::INTELLIGENCE << "\n"
-				<< (int)BuffType::WISDOM << " - " << BuffType::WISDOM << "\n"
-				<< (int)BuffType::ARMOR_CLASS << " - " << BuffType::ARMOR_CLASS << "\n"
-				<< (int)BuffType::STRENGTH << " - " << BuffType::STRENGTH << "\n"
-				<< (int)BuffType::CHARISMA << " - " << BuffType::CHARISMA << "\n"
-				<< (int)BuffType::CONSTITUTION << " - " << BuffType::CONSTITUTION << "\n"
-				<< (int)BuffType::DEXTERITY << " - " << BuffType::DEXTERITY << "\n"
-				<< (int)BuffType::ATTACK_BONUS << " - " << BuffType::ATTACK_BONUS << "\n"
-				<< (int)BuffType::DAMAGE_BONUS << " - " << BuffType::DAMAGE_BONUS << "\n"
-				<< "-1 to stop\n";
-			cout << "Enter choice: ";
-			cin >> choiceBuff;
-
-			//! Check for buff choice. Ask again if incorrectly entered
-			while (choiceBuff != -1 && (choiceBuff < 0 || choiceBuff>8)) {
-				cout << "\nIncorrect Input. Please enter a correct number: ";
-				cin >> choiceBuff;
-			}
-
-			//! If do not want to exit
-			if (choiceBuff != -1) {
-				int amount = 0;
-				//! Ask for buff amount
-				cout << "Enter buff's amount: ";
-				cin.ignore();
-				cin >> amount;
-				//! Put in vector for buff
-				vecBuff.push_back(Buff((BuffType)choiceBuff, amount));
-			}
-
-		}
-	}
-
-	Item newItem; //! Item variable
-	Weapon newWeapon; //! Weapon variable
-	if (choiceItem == 0) { //! If item or weapon
-		//! Create and validate item
-		newItem = Item((ItemType)itemType, name, vecBuff);
-		if (newItem.validateItem()) {
-			cout << "\nHere is the item that you want to add: \n";
-			cout << newItem;
-		}
-		else {
-			//! If not correct item. return back.
-			cout << "\nIncorrect Item. Item Refused.";
-			return;
-		}
-	}
-	else {
-		//! Create and validae weapon
-		newWeapon = Weapon(ItemType::WEAPON, name, vecBuff, range);
-		if (newWeapon.validateWeapon()) {
-			cout << "\nHere is the weapon that you want to add: \n";
-			cout << newWeapon;
-		}
-		else {
-			//! If invalid weapon. do not add to backpack and return
-			cout << "\nIncorrect Weapon. Weapon Refused.";
-			return;
-		}
-	}
-
-	//! Ask for user confirmation to add in bakcpack
-	string contAns = "";
-	cout << "\nDo you want to add the item to the backpack? (Y/N) ";
-	cin >> contAns;
-
-	//! Check input
-	while (contAns != "Y" && contAns != "N") {
-		cout << "\nPlease enter Y or N: ";
-		cin >> contAns;
-	}
-
-	//! If wan to continue, then store in backpack
-	if (contAns == "Y") {
-		if (choiceItem == 0)
-			chr.storeItem(&newItem);
-		else
-			chr.storeItem(&newWeapon);
-	}
-}
-
+//! method that takes the string representation of an Item object and saves to a text file
 void Item::saveItem(){
 	ofstream outItem;
 	outItem.open("saveFiles/Items/" + itemName + ".txt");
--- COMP_345_Project/ItemUtils.h
+++ COMP_345_Project/ItemUtils.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "Item.h"
+
+//! Free function to create a new item
 void createItem();
+
+//! Free Function to edit an Item File
 void editItem();
+
+//! Free function that reads an item file
+//! return: pointer to the newly read item
 Item* readItemFile(string itemName);
