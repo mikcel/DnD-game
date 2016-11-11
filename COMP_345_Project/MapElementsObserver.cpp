1d01bc0f9cfde605900c4fcf1658dce7641fb2eb
andrey-titov <andreit@videotron.ca>
2016-11-10 19:33:02 -0500

    console toggle

--- COMP_345_Project/MapElementsObserver.cpp
+++ COMP_345_Project/MapElementsObserver.cpp
@@ -1,6 +1,7 @@
 #include "MapElementsObserver.h"
 #include "Map.h"
 #include "ConsoleLogger.h"
+#include "Chest.h"
 #include <iostream>
 
 MapElementsObserver::MapElementsObserver(Map* newMap) : map(*newMap)
@@ -17,19 +18,29 @@ bool MapElementsObserver::toggle()
 	{
 		isShown = true;
 
-		Player& player = map.getPlayer();
-		std::cout << player.getCharacter();
+		// Show player stats
+		Player* player = &map.getPlayer();
+		std::cout << player->getCharacter();
 		
-		//for (Element* element : map.getElements())
-		//{
-		//	CharacterElement* characterElement = dynamic_cast<CharacterElement*>(element);
-		//	if (characterElement)
-		//	{
-		//		characterElement->getCharacter().attach(*this);
-		//		//characterElement->attach(*this);
-		//	}
+		// Show other characters' stats
+		for (Element* element : map.getElements())
+		{
+			CharacterElement* characterElement = dynamic_cast<CharacterElement*>(element);
+			if (characterElement && player != characterElement)
+			{
+				std::cout << characterElement->getCharacter();
+			}
+		}
 
-		//}
+		// Show chests' info
+		for (Element* element : map.getElements())
+		{
+			Chest* chest = dynamic_cast<Chest*>(element);
+			if (chest)
+			{
+				std::cout << *chest;
+			}
+		}
 
 		return true;
 	}
